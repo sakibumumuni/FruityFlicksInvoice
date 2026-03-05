4e0369dee386e5e67802d5cf4c8e7801a6452df5
Sakibu Mumuni <sakibumumuni285@gmail.com>
2026-03-05 15:00:50 +0000

    Added an API endpoint to collect data from the invoice template

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ print(client.list_database_names())
 @app.route('/company_data', methods=['POST', 'GET'])
 def get_user_data():
         if request.method == 'POST':
-        # get data from the form
+        # get data from the , this is to send the company's details into our databse, so that we have access to their basic information, in case we deploy the system for them,
+        # or better still to help keep track of anyone from the company who logs in to the sytem, so the company holds them accountable for any invoice they generated 
          user_data = {
             'companyname': request.form.get('companyname'),
             'emailaddress': request.form.get('emailaddress'),
@@ -23,11 +24,29 @@ def get_user_data():
             'address': request.form.get('address'),
             'taxid': request.form.get('taxid'),
             'financialyear-startdate': request.form.get('financialyear-startdate'), 
-            'financialyear-enddate': request.form.get('financialyear-enddate')
+            'financialyear-enddate': request.form.get('financialyear-enddate'),
+            'companylogo':request.form.get('companylogo')
          }
          CODE_OS.insert_one(user_data)
         return render_template('base.html')
+@app.route('/invoice', methods=['POST', 'GET'])
+def invoice():
+   invoice_data = {
+      'clientname':request.form.get('clientname'),
+      'clientemail':request.form.get('clientemail'),
+      'clientaddress':request.form.get('clientaddress'),
+     'invoice_number':request.form.get('invoicenumber'),
+    'issuedate':request.form.get('issuedate'),
+    'duedate':request.form.get('duedate'),
+    'itemdescription':request.form.get('itemdescription'),
+    'quantity':request.form.get('quantity'),
+    'unitprice':request.form.get('unitprice'),
+    'subtotal':request.form.get('subtotal'),
+    'tax':request.form.get('tax'),
+    'total':request.form.get('total')
 
+
+           }
 if __name__ == '__main__':
  port = int(os.environ.get('PORT', 5000))
  app.run(debug=True, host='0.0.0.0', port=port)
